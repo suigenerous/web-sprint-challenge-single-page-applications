5728d118f1849ad766ab88821daca815282b3b0d
William Herman <williamajherman@gmail.com>
2020-08-21 13:14:10 -0400

    added form inputs

--- src/App.js
+++ src/App.js
@@ -7,10 +7,66 @@ import formSchema from './validation/formSchema'
 import axios from 'axios'
 import * as yup from 'yup'
 
+const initialFormValues = {
+  name: '',
+  pizzaSize: '',
+  toppings: {
+    pepperoni: false,
+    sausage: false,
+    olives: false,
+    jalapenos: false,
+  },
+  specialInstructions: ''
+}
+
+const initialFormErrors = {
+  name: ''
+}
+
+const initialOrders = []
+const initialDisabled = true
+
 const App = () => {
+
+  // set states
+
+  const [orders, setOrders] = useState(initialOrders)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled) 
+
+  // networking helpers
+
+  const getOrders = () =>{
+    // axios call to back end 
+  }
+
+  const postNewOrder = (newOrder) => {
+    // axios post to backend
+    console.log(newOrder)
+  }
+  // form helpers
+
+  const inputChange = (name, value) => {
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const checkboxChange = (name, isChecked) => {
+    setFormValues({...formValues, toppings: {...formValues.toppings, [name]: isChecked,}})
+  }
+
+  const submit = () => {
+    const newOrder = {
+      name: formValues.name,
+      pizzaSize: formValues.pizzaSize,
+      toppings: Object.keys(formValues.toppings).filter(top => formValues.hobbies[top]),
+    }
+    postNewOrder(newOrder)
+  }
+
   return (
     <div className = 'App'>
-      <div class= 'nav-container'>
+      <div className= 'nav-container'>
         <h1>Lambda Eats</h1>
         <nav>
           <Link to='/'>Home</Link>
@@ -20,10 +76,19 @@ const App = () => {
       </div>
       <Switch>
         <Route path='/OrderStatus'>
-          <OrderStatus/>
+          <OrderStatus
+            orders = {orders}
+          />
         </Route>
         <Route path='/Form'>
-          <Form/>
+          <Form
+            values={formValues}
+            inputChange={inputChange}
+            checkboxChange={checkboxChange}
+            submit={submit}
+            disabled={disabled}
+            errors={formErrors}
+          />
         </Route>
         <Route path='/'>
           <HomePage/>
--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,74 @@
 import React from "react";
 
 export default function Form(props) {
+    // props
+
+    const {
+        values,
+        inputChange,
+        checkboxChange,
+        submit,
+        disabled,
+        errors,
+    } = props
+    //helper functions
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+    const onCheckboxChange = evt => {
+        const { name, checked } = evt.target
+        checkboxChange(name, checked)
+      }
+    
+      const onInputChange = evt => {
+        const { name, value } = evt.target
+        inputChange(name, value)
+      }
+
+    // JSX
+
     return (
-        <div className = 'formContainer'>
-            <h1>Form</h1>
+        <div className = 'formContainer' onSubmit={onSubmit}>
+            <h2>Form</h2>
+            <div className='form-group submit'>
+                <h3>Add an Order</h3>
+
+                <button disabled={disabled}>submit order</button>
+
+                <div className='errors'>
+                    <div>{errors.username}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.role}</div>
+                    <div>{errors.civil}</div>
+                </div>
+            </div>
+            <div className = 'form-inputs'>
+                {/* order name text input*/}
+                <label>Name&nbsp;
+                    <input
+                        value = {values.name}
+                        onChange = {onInputChange}
+                        name = 'name'
+                        type = 'text'
+                    />
+                </label>
+                {/* pizza size drop down */}
+                <label>Pizza Size&nbsp;
+                    <select
+                        value={values.pizzaSize}
+                        onChange={onInputChange}
+                        name='pizzaSize'
+                    >
+                        <option value=''>- Select an option -</option>
+                        <option value='14'>14"</option>
+                        <option value='16'>16"</option>
+                        <option value='18'>18"</option>
+                    </select>
+                </label>
+            </div>
+
         </div>
     )
 }
