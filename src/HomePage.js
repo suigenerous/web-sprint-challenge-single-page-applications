1ab803269212b8e7c7249925a348cbdef5cb6a67
William Herman <williamajherman@gmail.com>
2020-08-21 12:14:26 -0400

    file structure

---
+++ src/HomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomePage(props) {
+    return (
+        <div className = 'homePageContainer'>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
---
+++ src/OrderStatus.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OrderStatus(props) {
+    return (
+        <div className = 'orderStatusContainer'>
+            <h1>Order Status</h1>
+        </div>
+    )
+}
